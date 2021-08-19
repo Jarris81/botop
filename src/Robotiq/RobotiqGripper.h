fde863eb021bd78fbdb34b42ce775fa92223bba5
Jason Harris <harris.e.jason@gmail.com>
2021-08-19 14:47:53 +0200

    updated robotiq gripper with init string and header

--- src/Robotiq/RobotiqGripper.h
+++ src/Robotiq/RobotiqGripper.h
@@ -1,87 +1,26 @@
 #pragma once
 
-<<<<<<< HEAD
-//#include <Core/util.h>
-#include <Core/thread.h>
-=======
->>>>>>> marc/master
 #include <Franka/controlEmulator.h>
 #include <boost/asio/serial_port.hpp>
 #include <boost/asio.hpp>
 
-<<<<<<< HEAD
-// all values taken from docs of gripper
-const double MAX_WIDTH = 0.85; // in mm
-
-const double MIN_FORCE = 20;
-const double MAX_FORCE = 235; // in Newton
-
-const double MIN_SPEED = 0.02;
-const double MAX_SPEED = 0.15; // in  mm/s
-
-using namespace boost::asio;
-
-namespace robotiq{
-    class Gripper;
-}
-
-//The control message send to the robot
-struct GripperCmdMsg {
-    enum Command { _open, _close, _home, _done };
-    Command cmd=_done;
-    double force=20;  //which is 1kg
-    double width=.05; //which is 5cm
-    double speed=.1;
-};
-
-struct RobotiqGripper : rai::GripperAbstraction, Thread{
-    Var<GripperCmdMsg> cmd;
-
-//    serial_port port;
+struct RobotiqGripper : rai::GripperAbstraction {
     double maxWidth;
 
     RobotiqGripper(uint whichRobot=0);
-    ~RobotiqGripper(){ threadClose(); }
+    ~RobotiqGripper();
 
-    void homing();
+    void goTo(double force, double width, double speed);
 
-    void open(double width=.85, //which is 7.5cm
-              double speed=.2);
-
-    void close(double force=20,  //which is 1kg
-               double width=.05, //which is 5cm
-               double speed=.1);
+    void open(double width, double speed){ goTo(0., width, speed); }
+    void close(double force, double width, double speed){ goTo(force, width, speed); }
 
     double pos();
 
-    bool isGrasped();
-
-    void step();
-
-    void waitForIdle();
+    bool isDone();
 
 private:
-    std::shared_ptr<serial_port> serialPort;
+    std::shared_ptr<boost::asio::serial_port> serialPort;
 
-=======
-struct RobotiqGripper : rai::GripperAbstraction {
-  double maxWidth;
-
-  RobotiqGripper(uint whichRobot=0);
-  ~RobotiqGripper();
-
-  void goTo(double force, double width, double speed);
-
-  void open(double width, double speed){ goTo(0., width, speed); }
-  void close(double force, double width, double speed){ goTo(force, width, speed); }
-
-  double pos();
-
-  bool isDone();
-
-private:
-  std::shared_ptr<boost::asio::serial_port> serialPort;
-
-  void getStatus();
->>>>>>> marc/master
+    void getStatus();
 };
